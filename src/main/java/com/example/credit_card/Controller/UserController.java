8d5872b9a4d5a770c293dcd01c97501d45ffdeb2
Swadesh Parida <swadeshkuparida99@gmail.com>
2026-09-04 21:40:50 +0530

    User Api Added

--- src/main/java/com/example/credit_card/Controller/UserController.java
+++ src/main/java/com/example/credit_card/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.credit_card.Controller;
 
 import com.example.credit_card.Entity.UserEntity;
+import com.example.credit_card.Service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/infoPage")
 public class UserController {
 
-    private UserEntity userEntity;
-    public UserController(UserEntity userEntity){
-        this.userEntity = userEntity;
+
+    private UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService;
     }
 
     @PostMapping("/user")
     public void userInfo(@RequestBody UserEntity userInfo){
-
+        userService.saveUser(userInfo);
     }
 }
